fac81d42f2b39b7a556360f75acff161de6b7c39
Heberty Guelfi do Prado <hebertyprado22005@gmail.com>
2023-01-03 10:03:47 -0300

    the mat-ink-bar solution was implemented in product component

--- src/app/pages/product/product.component.ts
+++ src/app/pages/product/product.component.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { MatTabGroup } from '@angular/material/tabs';
+import { Subscription } from 'rxjs';
+import { InteractionService } from 'src/app/resources/interaction.service';
 
 @Component({
   selector: 'app-product',
@@ -7,4 +10,25 @@ import { Component } from '@angular/core';
 })
 export class ProductComponent {
 
+  valor: boolean;
+  subscription: Subscription;
+  @ViewChild('MatTabGroup') tabGroup: MatTabGroup;
+
+  constructor(private interactionService: InteractionService){
+  }
+
+  ngOnInit(): void {
+    this.subscription = this.interactionService.testeApenas$
+      .subscribe(valor => {
+        this.valor = valor;
+        setTimeout(() => {
+          this.tabGroup.realignInkBar();
+          }, 200)
+      });
+
+  }
+
+  ngOnDestroy() {
+    this.subscription && this.subscription.unsubscribe();
+  }
 }
